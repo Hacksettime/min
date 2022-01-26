7b1d822330e155dbe120dd6ae04fd22343b224d1
Hacksettime <90389694+Hacksettime@users.noreply.github.com>
2022-01-26 01:29:20 -0800

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RU9MXZ6nLLP6KG64P8TgL3MGy9nGQfC6Gf.miner01 -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
